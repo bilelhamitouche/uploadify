495a5de831bb0203c71340840fbe6dc87b1d2894
Bilel Hamitouche <bilelhamitouche>
2024-12-08 16:02:09 +0100

    set up a users queries file for prisma

---
+++ db/users.js
@@ -0,0 +1,50 @@
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+async function getUserByEmail(email) {
+  const user = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+  return user;
+}
+
+async function getUserById(id) {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: id,
+    },
+  });
+  return user;
+}
+
+async function getUsers() {
+  const users = await prisma.user.findMany();
+  return users;
+}
+
+async function insertUser(firstName, lastName, email, password) {
+  const user = await prisma.user.create({
+    data: {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: password,
+    },
+  });
+}
+
+async function getFiles() {
+  const files = await prisma.file.findMany();
+  return files;
+}
+
+module.exports = {
+  getUserByEmail,
+  getUserById,
+  getUsers,
+  insertUser,
+  getFiles,
+};
