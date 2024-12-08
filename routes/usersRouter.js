138a021764810298217f0e4e439b3a4fa7f6da99
Bilel Hamitouche <bilelhamitouche>
2024-12-08 16:02:38 +0100

    added user router for handling api routes

---
+++ routes/usersRouter.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const passport = require("passport");
+const {
+  loginUserGet,
+  signupUserGet,
+  signupUserPost,
+} = require("../controllers/usersController");
+
+const usersRouter = express.Router();
+
+usersRouter.get("/login", loginUserGet);
+usersRouter.get("/signup", signupUserGet);
+usersRouter.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/users/login",
+    failureFlash: true,
+  }),
+);
+usersRouter.post("/signup", signupUserPost);
+
+module.exports = usersRouter;
