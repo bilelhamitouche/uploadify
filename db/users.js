c2bd02d46e77c090976090589b2f4f4ebbcf346f
Bilel Hamitouche <bilelhamitouche>
2024-12-11 01:13:43 +0100

    changed prisma client import to the new one

--- db/users.js
+++ db/users.js
@@ -1,6 +1,4 @@
-const { PrismaClient } = require("@prisma/client");
-
-const prisma = new PrismaClient();
+const prisma = require("../prisma/prismaClient");
 
 async function getUserByEmail(email) {
   const user = await prisma.user.findUnique({
