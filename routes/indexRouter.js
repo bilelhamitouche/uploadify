0fa9d95be57ae6dfd2a86402b0d745bb259ca544
Bilel Hamitouche <bilelhamitouche>
2024-12-11 01:13:25 +0100

    mounted the folder router in the index router

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const { homepageGet } = require("../controllers/indexController");
+const folderRouter = require("./folderRouter");
 
 const indexRouter = express.Router();
 
 indexRouter.get("/", homepageGet);
+
+indexRouter.use("/root/folders", folderRouter);
 
 module.exports = indexRouter;
