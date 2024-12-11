f734256eaef8cf53cfc39dd81e498f7b025de893
Bilel Hamitouche <bilelhamitouche>
2024-12-11 03:49:49 +0100

    added isAuthenticated variable for view

--- controllers/folderController.js
+++ controllers/folderController.js
@@ -2,7 +2,11 @@ const { getRootFolder } = require("../db/folders");
 
 async function displayRootFolder(req, res) {
   const rootFolder = await getRootFolder(req.user.id);
-  res.render("userFolder", { user: req.user, rootFolder: rootFolder });
+  res.render("userFolder", {
+    user: req.user,
+    rootFolder: rootFolder,
+    isAuthenticated: req.isAuthenticated(),
+  });
 }
 
 module.exports = {
