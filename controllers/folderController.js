0f3fd6932c0813a37f720e42ae3a6c68adc1194f
Bilel Hamitouche <bilelhamitouche>
2024-12-16 00:39:34 +0100

    added a controller for creating a folder

--- controllers/folderController.js
+++ controllers/folderController.js
@@ -1,4 +1,4 @@
-const { getRootFolder } = require("../db/folders");
+const { getRootFolder, insertFolder } = require("../db/folders");
 
 async function displayRootFolder(req, res) {
   const rootFolder = await getRootFolder(req.user.id);
@@ -10,6 +10,19 @@ async function displayRootFolder(req, res) {
   });
 }
 
+async function createFolder(req, res) {
+  const { folder } = req.body;
+  if (req.params.folderId === "root") {
+    const parentFolder = await getRootFolder(req.user.id);
+    await insertFolder(folder, parentFolder.id, req.user.id);
+    res.redirect("/root/folders");
+  } else {
+    await insertFolder(folder, req.folderId, req.user.id);
+    res.redirect(`/root/folders/${req.params.folderId}`);
+  }
+}
+
 module.exports = {
   displayRootFolder,
+  createFolder,
 };
