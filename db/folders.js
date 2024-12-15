1c4a65228e99aeb1e96d4b7df1855d9f41cc0cac
Bilel Hamitouche <bilelhamitouche>
2024-12-16 00:39:21 +0100

    added a insertFolder function

--- db/folders.js
+++ db/folders.js
@@ -1,21 +1,52 @@
 const prisma = require("../prisma/prismaClient");
 
 async function getRootFolder(userId) {
-  const rootFolder = await prisma.folder.findUnique({
-    where: {
-      name_userId: {
-        name: "root",
-        userId: userId,
+  try {
+    const rootFolder = await prisma.folder.findUnique({
+      where: {
+        name_userId: {
+          name: "root",
+          userId: userId,
+        },
       },
-    },
-    include: {
-      subfolders: true,
-      files: true,
-    },
-  });
-  return rootFolder;
+      include: {
+        subfolders: true,
+        files: true,
+      },
+    });
+    return rootFolder;
+  } catch (err) {
+    console.log(err);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+async function insertFolder(name, parentFolderId, userId) {
+  try {
+    const folder = await prisma.folder.create({
+      data: {
+        name: name,
+        User: {
+          connect: {
+            id: userId,
+          },
+        },
+        parentFolder: {
+          connect: {
+            id: parentFolderId,
+          },
+        },
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  } finally {
+    await prisma.$disconnect();
+  }
 }
 
 module.exports = {
   getRootFolder,
+  insertFolder,
 };
