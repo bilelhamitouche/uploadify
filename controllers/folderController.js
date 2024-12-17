53ed5294e18e888d37a234645b5e4865d20ae618
Bilel Hamitouche <bilelhamitouche>
2024-12-17 19:04:14 +0100

    fixed a bug in createFolder handler

--- controllers/folderController.js
+++ controllers/folderController.js
@@ -1,4 +1,8 @@
-const { getRootFolder, insertFolder } = require("../db/folders");
+const {
+  getRootFolder,
+  insertFolder,
+  getCurrentFolder,
+} = require("../db/folders");
 
 async function displayRootFolder(req, res) {
   const rootFolder = await getRootFolder(req.user.id);
@@ -17,12 +21,28 @@ async function createFolder(req, res) {
     await insertFolder(folder, parentFolder.id, req.user.id);
     res.redirect("/root/folders");
   } else {
-    await insertFolder(folder, req.folderId, req.user.id);
+    await insertFolder(folder, req.params.folderId, req.user.id);
     res.redirect(`/root/folders/${req.params.folderId}`);
   }
 }
 
+async function displayCurrentFolder(req, res) {
+  const currentFolder = await getCurrentFolder(
+    req.user.id,
+    req.params.folderId,
+  );
+  res.render("currentFolder", {
+    user: req.user,
+    folderId: currentFolder.id,
+    folders: currentFolder.subfolders,
+    files: currentFolder.files,
+    isAuthenticated: req.isAuthenticated(),
+    previousPage: req.get("Referer"),
+  });
+}
+
 module.exports = {
   displayRootFolder,
+  displayCurrentFolder,
   createFolder,
 };
