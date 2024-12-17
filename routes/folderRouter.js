0c21a0940c796a8ca038efd191a175dcd4264817
Bilel Hamitouche <bilelhamitouche>
2024-12-17 19:04:36 +0100

    fixed creating nested folders

--- routes/folderRouter.js
+++ routes/folderRouter.js
@@ -2,12 +2,14 @@ const express = require("express");
 const {
   displayRootFolder,
   createFolder,
+  displayCurrentFolder,
 } = require("../controllers/folderController");
 const { ensureAuthenticated } = require("../controllers/authController");
 
 const folderRouter = express.Router();
 
 folderRouter.get("/", ensureAuthenticated, displayRootFolder);
+folderRouter.get("/:folderId/", ensureAuthenticated, displayCurrentFolder);
 folderRouter.post("/:folderId/create", ensureAuthenticated, createFolder);
 
 module.exports = folderRouter;
