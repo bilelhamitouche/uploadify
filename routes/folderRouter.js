c76203a50fc5c95e52a41c81df724fe0759b8960
Bilel Hamitouche <bilelhamitouche>
2024-12-18 01:39:31 +0100

    added a upload file route

--- routes/folderRouter.js
+++ routes/folderRouter.js
@@ -5,11 +5,13 @@ const {
   displayCurrentFolder,
 } = require("../controllers/folderController");
 const { ensureAuthenticated } = require("../controllers/authController");
+const { uploadFile } = require("../controllers/fileController");
 
 const folderRouter = express.Router();
 
 folderRouter.get("/", ensureAuthenticated, displayRootFolder);
 folderRouter.get("/:folderId/", ensureAuthenticated, displayCurrentFolder);
 folderRouter.post("/:folderId/create", ensureAuthenticated, createFolder);
+folderRouter.post("/:folderId/upload", ensureAuthenticated, uploadFile);
 
 module.exports = folderRouter;
