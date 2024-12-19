83c9421ccf878da88e040db0047669d15ecdab20
Bilel Hamitouche <bilelhamitouche>
2024-12-19 21:41:33 +0100

    added delete folder route and mounted file router

--- routes/folderRouter.js
+++ routes/folderRouter.js
@@ -3,9 +3,11 @@ const {
   displayRootFolder,
   createFolder,
   displayCurrentFolder,
+  deleteFolderPost,
 } = require("../controllers/folderController");
 const { ensureAuthenticated } = require("../controllers/authController");
 const { uploadFile } = require("../controllers/fileController");
+const { fileRouter } = require("./fileRouter");
 
 const folderRouter = express.Router();
 
@@ -13,5 +15,7 @@ folderRouter.get("/", ensureAuthenticated, displayRootFolder);
 folderRouter.get("/:folderId/", ensureAuthenticated, displayCurrentFolder);
 folderRouter.post("/:folderId/create", ensureAuthenticated, createFolder);
 folderRouter.post("/:folderId/upload", ensureAuthenticated, uploadFile);
+folderRouter.post("/:folderId/delete", ensureAuthenticated, deleteFolderPost);
+folderRouter.use("/:folderId/files", fileRouter);
 
 module.exports = folderRouter;
