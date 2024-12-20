64ede161ad76930e0bbf21e90494dd78a7a3f3c1
Bilel Hamitouche <bilelhamitouche>
2024-12-20 22:13:17 +0100

    added rename folder route

--- routes/folderRouter.js
+++ routes/folderRouter.js
@@ -4,6 +4,7 @@ const {
   createFolder,
   displayCurrentFolder,
   deleteFolderPost,
+  renameFolderPost,
 } = require("../controllers/folderController");
 const { ensureAuthenticated } = require("../controllers/authController");
 const { uploadFile } = require("../controllers/fileController");
@@ -16,6 +17,7 @@ folderRouter.get("/:folderId/", ensureAuthenticated, displayCurrentFolder);
 folderRouter.post("/:folderId/create", ensureAuthenticated, createFolder);
 folderRouter.post("/:folderId/upload", ensureAuthenticated, uploadFile);
 folderRouter.post("/:folderId/delete", ensureAuthenticated, deleteFolderPost);
+folderRouter.post("/:folderId/rename", ensureAuthenticated, renameFolderPost);
 folderRouter.use("/:folderId/files", fileRouter);
 
 module.exports = folderRouter;
