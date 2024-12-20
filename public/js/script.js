bd9ae92d24b6c567149cf879d393ad9e3f861973
Bilel Hamitouche <bilelhamitouche>
2024-12-21 00:16:12 +0100

    made every button connect with it's rename modal

--- public/js/script.js
+++ public/js/script.js
@@ -6,10 +6,13 @@ const createFolderModalBtn = document.querySelector("#create-folder-modal-btn");
 const closeFolderModalBtn = document.querySelector("#close-folder-modal-btn");
 const uploadFileModal = document.querySelector("#upload-file-modal");
 const createFolderModal = document.querySelector("#create-folder-modal");
-const renameFolderBtn = document.querySelector("#rename-folder-btn");
-const renameFolderModal = document.querySelector("#rename-folder-modal");
-const closeFolderRenameModalBtn = document.querySelector(
+const renameFolderBtns = document.querySelectorAll("#rename-folder-btn");
+const closeFolderRenameModalBtns = document.querySelectorAll(
   "#close-folder-rename-modal-btn",
+);
+const renameFileBtns = document.querySelectorAll("#rename-file-btn");
+const closeFileRenameModalBtns = document.querySelectorAll(
+  "#close-file-rename-modal-btn",
 );
 
 avatarBtn.addEventListener("click", () => {
@@ -35,8 +38,24 @@ createFolderModalBtn.addEventListener("click", () => {
   createFolderModal.showModal();
 });
 
-renameFolderBtn.addEventListener("click", () => {
-  renameFolderModal.showModal();
+renameFolderBtns.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    const modalId = btn.dataset.modalId;
+    const modal = document.getElementById(modalId);
+    if (modal) {
+      modal.showModal();
+    }
+  });
+});
+
+renameFileBtns.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    const modalId = btn.dataset.modalId;
+    const modal = document.getElementById(modalId);
+    if (modal) {
+      modal.showModal();
+    }
+  });
 });
 
 closeFileModalBtn.addEventListener("click", () => {
@@ -46,7 +65,6 @@ closeFileModalBtn.addEventListener("click", () => {
     uploadFileModal.classList.remove("closing");
   }, 500);
 });
-
 closeFolderModalBtn.addEventListener("click", () => {
   createFolderModal.classList.add("closing");
   setTimeout(() => {
@@ -54,10 +72,29 @@ closeFolderModalBtn.addEventListener("click", () => {
     createFolderModal.classList.remove("closing");
   }, 500);
 });
-closeFolderRenameModalBtn.addEventListener("click", () => {
-  renameFolderModal.classList.add("closing");
-  setTimeout(() => {
-    renameFolderModal.close();
-    renameFolderModal.classList.remove("closing");
-  }, 500);
+closeFolderRenameModalBtns.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    const modalId = btn.dataset.modalId;
+    const modal = document.getElementById(modalId);
+    if (modal) {
+      modal.classList.add("closing");
+      setTimeout(() => {
+        modal.close();
+        modal.classList.remove("closing");
+      }, 500);
+    }
+  });
+});
+closeFileRenameModalBtns.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    const modalId = event.target.dataset.modalId;
+    const modal = document.getElementById(modalId);
+    if (modal) {
+      modal.classList.add("closing");
+      setTimeout(() => {
+        modal.close();
+        modal.classList.remove("closing");
+      }, 500);
+    }
+  });
 });
